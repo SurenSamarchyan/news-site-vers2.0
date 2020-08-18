9541500e2669178c0f99ff24187a709006959fa7
Suren Samarchyan <za077060642>
2020-08-18 18:13:24 +0400

    first change

--- src/components/card.js
+++ src/components/card.js
@@ -1,15 +1,24 @@
 import React from "react";
+import Col from 'react-bootstrap/Col'
+import './card.css'
+
 
 function Card(props) {
 	return (
-			<a href={props.url}>
-				<div>
-					<h2>{props.title}</h2>
-					<img src={props.urlToImage } alt=''/>
-					<p>{props.description}</p>
-				</div>
-			</a>
+		<Col xs={12} sm={6} lg={3}>
+			<div  className='news-card-block'>
+				<a href={props.url}>
+					<div className='card'>
+						<h2>{props.title}</h2>
+						<div className='news-image' style={{backgroundImage: `url(${props.urlToImage})`}}>
+						</div>
+						<p>{props.description}</p>
+					</div>
+				</a>
+			</div>
+		</Col>
 	)
 }
 
 export default Card;
+
--- src/index.js
+++ src/index.js
@@ -1,32 +1,54 @@
 import ReactDOM from 'react-dom';
 import React from "react";
-import Card from "./components/card";
+import { useState } from 'react';
+// Data
 import newsListObject from "./newsObject";
+// Component
+import Card from "./components/card";
 import Input from "./components/input";
 
+//Style
+import 'bootstrap/dist/css/bootstrap.css';
+import Row from 'react-bootstrap/Row';
+import './index.css'
 
-
-function Bord (){
-	return(
+function HomePage() {
+	
+	const [inputValue, setInputValue ] = useState('');
+	
+	
+	const handleInputChanges = (event)=>{
+		setInputValue(event.target.value)
+		//getNews()
+	}
+	
+	return (
 		<div>
-			<Input />
-			{newsListObject.articles.map((news, index) => {
-			return (
-				<Card
-					key={ index }
-					url={ news.url }
-					title= { news.title }
-					urlToImage= { news.urlToImage }
-					description= {news.description}
-				/>
-			)
-			})}
+			<input onChange={handleInputChanges}>
+			</input>
+			<div>{inputValue}</div>
+			<div id='news-container' className='container-fluid'>
+				<Row>
+					{newsListObject.articles.map((news, index) => {
+						return (
+							<Card
+								key={index}
+								cardIndex={index}
+								url={news.url}
+								title={news.title}
+								urlToImage={news.urlToImage}
+								description={news.description}
+							/>
+						)
+					})}
+				</Row>
+			</div>
 		</div>
 	)
 }
 
 ReactDOM.render(
-	<Bord />,
+	<HomePage/>,
 	document.getElementById('root')
 );
 
